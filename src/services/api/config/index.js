96ab741584b807ee2827d3cb2246bb79798d17c8
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-08-16 22:02:43 -0300

    configuracao API

---
+++ src/services/api/config/index.js
@@ -0,0 +1,22 @@
+const baseUrl = "https://dog.ceo/api";
+
+axios.defaults.baseURL = baseUrl /* Global axios defaults */
+
+
+async function get(url) {
+
+  const Axiosinstance = axios;
+  
+  const retorno = await Axiosinstance({
+    method: 'get',
+    url: url,
+    responseType: 'json'
+  })
+    .catch(function(e) {
+      throw Error(e)
+    });
+
+return retorno
+}
+  
+
